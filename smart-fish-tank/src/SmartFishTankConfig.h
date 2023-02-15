39eae4b195c5df07c4ba43718012085fba1a9fbb
liangwenxu <liangwenxu@sinosoftgz.com.cn>
2023-02-15 18:02:49 +0800

    feat: 首页显示测试

--- smart-fish-tank/src/SmartFishTankConfig.h
+++ smart-fish-tank/src/SmartFishTankConfig.h
@@ -22,10 +22,12 @@
 #define TEMP_PIN 2
 
 // 外围设备控制引脚
-#define DEVIC_PUMP_PIN 0
-#define DEVIC_HEAT_PIN 1
-#define DEVIC_LIGHT_PIN 18
-#define DEVIC_EXT1_PIN 19
+#define DEVIC_PUMP_PIN 0 // 循环泵继电器控制脚
+#define DEVIC_HEAT_PIN 1 // 加热棒继电器控制脚
+#define DEVIC_LIGHT_PIN 18 // 灯具继电器控制脚
+#define DEVIC_O2_PIN 19 // 增氧泵继电器控制脚
+#define DEVIC_COOLER 11 // 24V制冷片控制脚
+
 
 // 温湿度传感器I2C定义
 #define SHTC3_I2C_SDA_PIN 8
--- smart-fish-tank/src/main.cpp
+++ smart-fish-tank/src/main.cpp
@@ -4,10 +4,23 @@
 #include "ui/pages.h"
 
 // U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
-// U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, I2C_SCL, I2C_SDA);
-// U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, I2C_SCL, I2C_SDA);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, I2C_SCL, I2C_SDA);
 
 int count = 0;
+
+static boolean wifiConnected = false;
+static boolean pumpOn = false;
+static boolean heaterOn = false;
+static boolean lightOn = false;
+static boolean o2On = false;
+static boolean coolerOn = false;
+static boolean feedMode = false;
+
+static int curTemp = 23; //当前水温
+static int targetTemp = 26; //目标水温
+static int airTemp = 28; //气温
+static int airHumidity = 70; //湿度
+static int tds = 0; //TDS数值
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,7 +32,38 @@ void setup() {
 }
 
 void loop() {
+  vector<StatusFlag> status;
   // put your main code here, to run repeatedly:
+  if (wifiConnected)
+  {
+    status.push_back(StatusFlag::WIFI_CONECTED);
+  }
+  if (pumpOn)
+  {
+    status.push_back(StatusFlag::PUMP_ON);
+  }
+  if (heaterOn = false)
+  {
+    status.push_back(StatusFlag::HEATER_ON);
+  }
+  if (lightOn = false)
+  {
+    status.push_back(StatusFlag::LIGHT_ON);
+  }
+  if (o2On = false)
+  {
+    status.push_back(StatusFlag::O2_ON);
+  }
+  if (coolerOn = false)
+  {
+    status.push_back(StatusFlag::COOLER_ON);
+  }
+  if (feedMode = false)
+  {
+    status.push_back(StatusFlag::FEEDMODE);
+  }
+  
+  draw_main_page(&u8g2, "17:30", curTemp, targetTemp, airTemp, airHumidity, tds, status);
 
   delay(2000);
 }
